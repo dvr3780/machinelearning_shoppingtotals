2faf8b7cd5bc06525d16b62766008eb2d754b838
Dan Raju <dvr3780@gmail.com>
2023-07-17 20:07:48 -0700

    commit machine learning shopping total calculate

---
+++ lib/components/grocery_item_tile.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+
+
+class GroceryItemTile extends StatelessWidget {
+  final String itemName;
+  String itemPrice;
+  final String imagePath;
+  final color;
+  bool textScanning = false;
+  XFile? imageFile;
+  String scannedText = "";
+  String scannedPrice = "";
+  final currencyFormat = NumberFormat("#,##0.00", "en_US");
+
+ 
+  void Function()? onPressed;
+
+  GroceryItemTile({
+    super.key,
+    required this.itemName,
+    required this.itemPrice,
+    required this.imagePath,
+    required this.color,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: color[100],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // item image
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Image.asset(
+                imagePath,
+                height: 40,
+              ),
+            ),
+
+            // item name
+            Text(
+              itemName,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              padding: const EdgeInsets.only(top: 10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.grey,
+                  shadowColor: Colors.grey[400],
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)),
+                ),
+                onPressed: () {
+                  getImage(ImageSource.camera);
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 5, horizontal: 5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.camera_alt,
+                        size: 1,
+                      ),
+                      Text(
+                        "Camera",
+                        style: TextStyle(
+                            fontSize: 7, color: Colors.grey[600]),
+                      )
+                    ],
+                  ),
+                ),
+            )),
+
+            MaterialButton(
+              onPressed: onPressed,
+              color: color,
+              child: Text(
+                'Add to Cart: \$' + itemPrice,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+ void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        textScanning = true;
+        imageFile = pickedImage;
+        final imageTemp = XFile(pickedImage.path);
+        //setState(() => imageFile = imageTemp);
+        getRecognisedText(imageTemp);//pickedImage);
+        //log(imageTemp.path);
+      }
+    } catch (e) {
+      textScanning = false;
+      imageFile = null;
+	  scannedText = "Error occured while scanning";
+      //setState(() =>{});
+    }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognisedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannedText = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = scannedText + line.text + "\n";
+      }
+    }
+
+    final priceRegex = RegExp(r'[\$\£\€]*(\d+(?:\.\d{1,2})?)', multiLine: true);
+    var priceString = priceRegex.allMatches(scannedText).map((m) => m.group(0));
+    if(priceString.isNotEmpty){
+      var price =  priceString.elementAt(0)!.replaceAll(RegExp(r'/(?:[$])\s*\d+(?:\.\d{2})?/'),'');
+      var valPrice = price.replaceAll('\$', '');
+      var p = double.parse(valPrice);
+      this.itemPrice = valPrice;
+      this.scannedPrice = valPrice;
+      //productPrices.add(p);
+      //total = total + p; 
+    }
+    textScanning = false;
+    //setState(() {});
+    //setState(() => {});
+  }
+
+  //@override
+ // void initState() {
+  //  super.initState();
+ // }
+
+}
---
+++ lib/model/cart_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../components/grocery_item_tile.dart';
+
+class CartModel extends ChangeNotifier {
+  // list of items on sale
+  final List _shopItems = const [
+    // [ itemName, itemPrice, imagePath, color ]
+    ["Avocado", "4.00", "lib/images/avocado.png", Colors.green],
+    ["Banana", "2.50", "lib/images/banana.png", Colors.yellow],
+    //["Chicken", "12.80", "lib/images/chicken.png", Colors.brown],
+    //["Water", "1.00", "lib/images/water.png", Colors.blue],
+  ];
+
+  
+  // list of cart items
+  List _cartItems = [];
+
+  get cartItems => _cartItems;
+
+  get shopItems => _shopItems;
+
+  // add item to cart
+  void addItemToCart(int index, String scannedPrice) {
+    //_cartItems.add(_shopItems[index]);
+    _cartItems.add([shopItems[index][0], scannedPrice, shopItems[index][2], shopItems[index][3]]);
+    notifyListeners();
+  }
+
+  // remove item from cart
+  void removeItemFromCart(int index) {
+    _cartItems.removeAt(index);
+    notifyListeners();
+  }
+
+  // calculate total price
+  String calculateTotal() {
+    double totalPrice = 0;
+    for (int i = 0; i < cartItems.length; i++) {
+      totalPrice += double.parse(cartItems[i][1]);
+    }
+    return totalPrice.toStringAsFixed(2);
+  }
+}
